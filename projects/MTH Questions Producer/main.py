b6f0afaecc5ded5a0717b19a7c49128beaa93369
BraydenJamesE <95278801+BraydenJamesE@users.noreply.github.com>
2023-03-17 17:24:07 -0700

    Functions added and update to output

--- projects/MTH Questions Producer/main.py
+++ projects/MTH Questions Producer/main.py
@@ -1,25 +1,41 @@
 import random
 
 
-def endProgram():
-    print("You entered the wrong value. Ending the program.")
+def endProgram(message = "You entered the wrong value. Ending the program."):
+    print(message)
     quit() 
 # end of "endProgram" function   
     
     
-def expandRange(beginRange, endRange):
-    rangeItems = []
+def isFloat(number): # function checking if value is a float
+    # checking int value
+    if isinstance(number, int):
+        return False
+    # checking float value
+    if isinstance(number, float):
+        return True
+    # checking string value
+    if number[0].isnumeric() or number[len(number) - 1].isnumeric(): # checking if fist value or last value is numeric 
+        if '.' in number: 
+            return True
+        else:
+            return False
+    else:
+        return False
+# end of "isFloat" function
+            
     
+def expandRange(beginRange, endRange): # takes in two values and outputs the range
+    rangeItems = []
     for i in range(int(beginRange),int(endRange) + 1):
         rangeItems.append(i)
-        
     return rangeItems
 # end of "expandRange" function        
         
         
 def sortProblemsIntoList(problems):
     newProblems = []
-
+    
     for arrayItem in problems:
         # variables
         beginNumberIndex = 0
@@ -113,49 +129,71 @@ def getInputFromUser(allProblems, problemsMarkedForReview):
     problemSelection = input("What database do you wish to study from? (enter 0 for review and non-review or 1 for review only): ")
     numberOfProblems = input("Please enter the number of problems you would like: ")
     
+    if isFloat(numberOfProblems): # alerting user if float was entered
+        print("You entered a float. Changing ", float(numberOfProblems), " to int: " , int(float(numberOfProblems)))  
+    
+    numberOfProblems = int(float(numberOfProblems)) # Changing number of Problems to integer
+    
+    if numberOfProblems <= 0: # ensuring that the value entered is larger than 0 and not a float
+        endProgram("Must enter a positive numberer of problems as integer. Ending Program.")
+    
+    
     # using the "sortProblemsIntoList" function to isolate each problem from the string
     allProblems = sortProblemsIntoList(allProblems)
     problemsMarkedForReview = sortProblemsIntoList(problemsMarkedForReview)
     
     allProblemsGreaterThanZero = len(allProblems) > 0
     problemsMarkedForReviewGreaterThanZero = len(problemsMarkedForReview) > 0
-    
+
     
     # Producing Problems 
     if allProblemsGreaterThanZero:
-            allProblemsProduced = produceProblems(allProblems, int(numberOfProblems))
+            allProblemsProduced = produceProblems(allProblems, numberOfProblems)
         
     if problemsMarkedForReviewGreaterThanZero:
-        problemsMarkedForReviewProduced = produceProblems(problemsMarkedForReview, int(numberOfProblems))
+        problemsMarkedForReviewProduced = produceProblems(problemsMarkedForReview, numberOfProblems)
     
     
     # Printing List to User
     if problemSelection == '0': # user wants all problems
         if problemsMarkedForReviewGreaterThanZero:
-            for i in problemsMarkedForReviewProduced:
-                print(i , end = " ") 
-        if allProblemsGreaterThanZero:
-            for i in allProblemsProduced:
-                print(i , end = " ")           
+            i = 0
+            j = 0
+            count = 0
+            while count < numberOfProblems:
+                
+                if i < len(problemsMarkedForReviewProduced):
+                    print(problemsMarkedForReviewProduced[i], end = " ")
+                    i += 1
+                    count += 1
+                elif j < len(allProblemsProduced):
+                    print(allProblemsProduced[j], end = " ")
+                    j += 1
+                    count += 1
+                else:
+                    break
+            
+            
+        #     for i in problemsMarkedForReviewProduced:
+        #         print(i , end = " ") 
+        # if allProblemsGreaterThanZero:
+        #     for i in allProblemsProduced:
+        #         print(i , end = " ")           
     
     elif problemSelection == '1': # user wants only problems marked for review
         if problemsMarkedForReviewGreaterThanZero:
             for i in problemsMarkedForReviewProduced:
                 print(i , end = " ") 
         else:
-            print("Sorry; there are no review problems.")
-            quit()        
+            endProgram("Sorry; there are no review problems.")      
    
     else: 
         endProgram()
-        
-    
-    
-    
+# end of "getInputFromUser" function       
+
 #"4.10: 470-472, 474-477, 479, 481, 484, 490-497, 499-503", "1.1: 2-11, 42-43", "1.1: 12-15, 20-21, 24-26"
 # String Array of Homework Problems    
 allproblems = ["4.10: 470-472, 474-477, 479, 481, 484, 490-497, 499-503", "1.1: 2-11, 42-43", "1.1: 12-15, 20-21, 24-26", "Quiz3: 1-10"]
 problemsMarkedForReview = ["Quiz3: 1-10"]
 newProblems = sortProblemsIntoList(allproblems)
-
 getInputFromUser(allproblems, problemsMarkedForReview)
