6d547531b0a417d2338c00aaa6983d57e7b32c9a
BraydenJamesE <95278801+BraydenJamesE@users.noreply.github.com>
2023-03-23 23:52:58 -0700

    Program now allows the user to create a study plan

--- projects/MTH Questions Producer/main.py
+++ projects/MTH Questions Producer/main.py
@@ -1,7 +1,11 @@
+import numpy as np
 import random
+import math
 
+# Global Variables
+unitDictionary = {} # Dictionary that tracks the unit assocaited with each problem
 
-def endProgram(message = "You entered the wrong value. Ending the program."):
+def endProgram(message = "You entered the wrong value. Ending the program."): # this function ends the program with a default or specific message
     print(message)
     quit() 
 # end of "endProgram" function   
@@ -24,13 +28,24 @@ def isFloat(number): # function checking if value is a float
         return False
 # end of "isFloat" function
 
-def removeDuplicates(listOfItemsToBeRemoved, listOfItemsBeingComparedWith): 
+
+def removeDuplicates(listOfItemsToBeRemoved, listOfItemsBeingComparedWith): # this function takes in two lists and removes the values that already exist in the second one out of the first. The second list does not lose any items, only the first.
     for i in listOfItemsBeingComparedWith:
         if i in listOfItemsToBeRemoved:
             listOfItemsToBeRemoved.remove(i)
     # end of for loop (i)
     return listOfItemsToBeRemoved
-# end of "removeDuplicates" function    
+# end of "removeDuplicates" function
+
+
+def createStudyPlan(numberOfDays, numberOfProblems, numberOfTimeToCoverEachProblem = 1): # This function allows the user to create a study plan.
+    if numberOfTimeToCoverEachProblem != 1:
+        numberOfProblemsPerDay = math.ceil((numberOfProblems * numberOfTimeToCoverEachProblem) / numberOfDays)
+        print("You must study", numberOfProblemsPerDay, "problems per day to be ready for your test if you do each problem", numberOfTimeToCoverEachProblem, "times.")
+    else:
+        numberOfProblemsPerDay = math.ceil(numberOfProblems / numberOfDays)
+        print("You must study", numberOfProblemsPerDay, "problems per day to be ready for your test.")
+# end of "createStudyPlan" function
             
     
 def expandRange(beginRange, endRange): # takes in two values and outputs the range
@@ -39,6 +54,8 @@ def expandRange(beginRange, endRange): # takes in two values and outputs the ran
         rangeItems.append(i)
     return rangeItems
 # end of "expandRange" function        
+        
+       
         
         
 def sortProblemsIntoList(problems):
@@ -53,12 +70,18 @@ def sortProblemsIntoList(problems):
         rangeFound = False
         rangeIndex = 0
         unitFound = False
+        listOfUnits = {}
         
         for iterator, stringItem in enumerate(arrayItem):
             # ASSESSING THE CHARACTERS IN THE STRING
             if stringItem == ':' and not beginNumberIndexFound:
                 unitFound = True
-                unit = arrayItem[beginNumberIndex : iterator]
+                unit = arrayItem[beginNumberIndex : iterator + 1] + " "
+                if unit not in listOfUnits: 
+                    listOfUnits.append(unit)
+                listOfUnits = np.unique(listOfUnits)
+                print("List: ", listOfUnits)
+
                 
             elif not beginNumberIndexFound and stringItem.isnumeric() and (unitFound or ':' not in arrayItem): # Tracking beginNumberIndex
                 beginNumberIndex = iterator
@@ -80,7 +103,7 @@ def sortProblemsIntoList(problems):
             # APPENDING ITEMS
             if not rangeFound and beginNumberIndexFound and endIndexFound: # appending value with no range
                 # Appending the item to the newProblems list
-                newProblems.append(arrayItem[beginNumberIndex : endIndex + 1]) 
+                newProblems.append(unit + arrayItem[beginNumberIndex : endIndex + 1]) 
                 
                 # resetting variables
                 endIndexFound = False 
@@ -92,7 +115,7 @@ def sortProblemsIntoList(problems):
                 rangeItems = expandRange(int(arrayItem[beginNumberIndex : rangeIndex].strip()), int(arrayItem[rangeIndex + 1 : endIndex + 1].strip()))
                 
                 for i in rangeItems: # looping through the range items to they can be added one-by-one
-                    newProblems.append(str(i))
+                    newProblems.append(unit + str(i))
                     
                 # resetting variables
                 endIndexFound = False 
@@ -137,11 +160,11 @@ def printList(problemsToPrint, numberOfItemsForPrint = None): # prints the value
                 numberOfProblemsDesiredAndNotPrinted = numberOfItemsForPrint - len(problemsToPrint)
                 numberOfItemsForPrint = len(problemsToPrint)
             for i in range(0,numberOfItemsForPrint): # looping through each item and printing
-                print(problemsToPrint[i], end = " ") 
+                print(problemsToPrint[i]) 
             # end of for loop (i)
         elif numberOfItemsForPrint == None: # if the number of items was not specified, printing the entire list and returning zero
             for j in problemsToPrint:
-                print(j , end = " ")
+                print(j)
             return 0
             # end of for loop (j)
     return numberOfProblemsDesiredAndNotPrinted # returning number of items desired but not printed due to smaller list size
@@ -149,8 +172,28 @@ def printList(problemsToPrint, numberOfItemsForPrint = None): # prints the value
 
 
 def getInputFromUser(allProblems, problemsMarkedForReview):
+    
+    # using the "sortProblemsIntoList" function to isolate each problem from the string
+    allProblems = sortProblemsIntoList(allProblems)
+    problemsMarkedForReview = sortProblemsIntoList(problemsMarkedForReview)
+
+    # shuffling values in the lists to random locations
+    allProblemsRandomized = randomizeList(allProblems)
+    problemsMarkedForReviewRandomized = randomizeList(problemsMarkedForReview)
+    
     # Taking input from the user
-    problemSelection = input("What database do you wish to study from? (enter 0 for review and non-review or 1 for review only): ")
+    problemSelection = input("What database do you wish to study from? (enter 0 for review and non-review, 1 for review only, and 2 for a study plan): ")
+    
+    if problemSelection == "2":
+        numberOfDaysUntilExam = int(input("Enter the number of days you have to study: "))
+        numberOfTimesToDoEachProblem = int(input("Enter the number of times you want to do each problem: "))
+        
+        if numberOfTimesToDoEachProblem <= 0 or numberOfDaysUntilExam <= 0:
+            endProgram("Error in the number you entered. Ending Program.")
+        else:
+            createStudyPlan(numberOfDaysUntilExam, len(allProblems), numberOfTimesToDoEachProblem)
+        endProgram("Thank you!")
+    
     numberOfProblems = input("Please enter the number of problems you would like: ")
     
     if isFloat(numberOfProblems): # alerting user if float was entered
@@ -161,14 +204,6 @@ def getInputFromUser(allProblems, problemsMarkedForReview):
     if numberOfProblems <= 0: # ensuring that the value entered is larger than 0 and not a float
         endProgram("Must enter a positive numberer of problems as integer. Ending Program.")
     
-    
-    # using the "sortProblemsIntoList" function to isolate each problem from the string
-    allProblems = sortProblemsIntoList(allProblems)
-    problemsMarkedForReview = sortProblemsIntoList(problemsMarkedForReview)
-
-    # shuffling values in the lists to random locations
-    allProblemsRandomized = randomizeList(allProblems)
-    problemsMarkedForReviewRandomized = randomizeList(problemsMarkedForReview)
 
     # Printing List to User
     if problemSelection == '0': # user wants all problems
@@ -176,7 +211,7 @@ def getInputFromUser(allProblems, problemsMarkedForReview):
         print("\n------------------------------------------\n")
         numberOfProblems = printList(problemsMarkedForReviewRandomized, numberOfProblems)
         numberOfProblems = printList(allProblemsRandomized, numberOfProblems)
-        print("\n\n------------------------------------------\n")
+        print("\n------------------------------------------\n")
         if numberOfProblems > 0:
                 print("Sorry. Number of problems desired is greater than the amount available. We printed what we could.") 
     elif problemSelection == '1': # user wants only problems marked for review
@@ -191,9 +226,12 @@ def getInputFromUser(allProblems, problemsMarkedForReview):
         endProgram("You must enter either a 0 or 1; ending program.")
 # end of "getInputFromUser" function       
 
+
 #"4.10: 470-472, 474-477, 479, 481, 484, 490-497, 499-503", "1.1: 2-11, 42-43", "1.1: 12-15, 20-21, 24-26"
 # String Array of Homework Problems    
 allproblems = ["4.10: 470-472, 474-477, 479, 481, 484, 490-497, 499-503", "1.1: 2-11, 42-43", "1.1: 12-15, 20-21, 24-26", "Quiz3: 100-120"]
 problemsMarkedForReview = ["Quiz3: 100-120"]
-newProblems = sortProblemsIntoList(allproblems)
+
 getInputFromUser(allproblems, problemsMarkedForReview)
+
+
